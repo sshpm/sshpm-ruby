f249e0d3b0d925b5df8a9da32774388219584466
Tercio de Melo <terciodemelo@gmail.com>
2017-02-18 13:45:05 -0300

    Adds SSHPM::Manager and SSHPM.manage implementation

--- lib/sshpm.rb
+++ lib/sshpm.rb
@@ -1,5 +1,17 @@
 require "sshpm/version"
+require "sshpm/manager"
 
 module SSHPM
-  # Your code goes here...
+  def self.manage(hosts = [], &block)
+    hosts = [hosts] unless hosts.is_a? Array
+
+    unless hosts.all? { |host| [String, Symbol].include? host.class }
+      raise TypeError('hosts must be either String or Symbols')
+    end
+
+    hosts.map(&:to_s).uniq.map do |host|
+      manager = Manager.new(host)
+      manager.instance_eval(&block)
+    end
+  end
 end
---
+++ lib/sshpm/manager.rb
@@ -0,0 +1,5 @@
+class SSHPM::Manager
+  def initialize(host)
+    @host = host
+  end
+end
