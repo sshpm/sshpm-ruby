3e9bbe0c33ae8ebc9b14dfaa0fb1ffc46a4e1439
Tercio de Melo <terciodemelo@gmail.com>
2017-02-24 10:01:06 -0300

    Makes SSHPM::Manager definition clearer and independent
    It is safer to externalize the module SSHPM to make the file itself
    more independent from others in the project

--- lib/sshpm/manager.rb
+++ lib/sshpm/manager.rb
@@ -1,27 +1,29 @@
 require 'sshpm/tasks'
 
-class SSHPM::Manager
-  attr_accessor :host, :tasks
+module SSHPM
+  class Manager
+    attr_accessor :host, :tasks
 
-  def initialize(host=nil)
-    @host = host
-    @tasks = []
-  end
-
-  def run_tasks
-    tasks.map do |task|
-      task.run_on(@host)
+    def initialize(host=nil)
+      @host = host
+      @tasks = []
     end
-  end
 
-  def add_user(&block)
-    task_builder = SSHPM::Tasks::Builder.new
-    task_builder.instance_eval(&block)
-    tasks << SSHPM::Tasks::AddUser.new(task_builder.attributes)
-  end
+    def run_tasks
+      tasks.map do |task|
+        task.run_on(@host)
+      end
+    end
 
-  def remove_user(&block)
-    task_builder = SSHPM::Tasks::Builder.build(&block)
-    tasks << SSHPM::Tasks::RemoveUser.new(task_builder.attributes)
+    def add_user(&block)
+      task_builder = SSHPM::Tasks::Builder.new
+      task_builder.instance_eval(&block)
+      tasks << SSHPM::Tasks::AddUser.new(task_builder.attributes)
+    end
+
+    def remove_user(&block)
+      task_builder = SSHPM::Tasks::Builder.build(&block)
+      tasks << SSHPM::Tasks::RemoveUser.new(task_builder.attributes)
+    end
   end
 end
